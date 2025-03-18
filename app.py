6e6a55385f26d89c3ce905bde47f48ee2351eaeb
Rajshah296 <ketakishah369@gmail.com>
2025-03-19 03:31:03 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def index():
     source_currency = data['queryResult']['parameters']['unit-currency']['currency']
     amount = data['queryResult']['parameters']['unit-currency']['amount']
     target_currency = data['queryResult']['parameters']['currency-name']
-    print('Hello')
+
 
     cf = fetch_conversion_factor(source_currency,target_currency)
     final_amount = amount * cf
@@ -28,7 +28,7 @@ def fetch_conversion_factor(source,target):
         Returns floating-point value.
     """
     client = currencyapicom.Client('cur_live_n0VkK3qX1ft6LiITu09ItilBS8rj9PZiMfMgX8D5')
-    exch_rate = client.latest(source,currencies=[target])
+    exch_rate = client.latest(source,currencies=target)
     return exch_rate['data'][f'{target}']['value']
 
 if __name__ == "__main__":
